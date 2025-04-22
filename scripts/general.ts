cd1403be98cb6c608d864ef31f04d491724db9d7
PixelFromTheMixery <pixelmixery@gmail.com>
2025-04-22 17:59:50 +1000

    fix rollback

--- scripts/general.ts
+++ scripts/general.ts
@@ -3,3 +3,21 @@ export async function fetchExample() {
     if (!res.ok) throw new Error("Failed to fetch");
     return await res.json();
 }
+
+export async function fetchDatabases() {
+    const res = await fetch("http://127.0.0.1:8001/api/v1/local/databases");
+    if (!res.ok) throw new Error("Failed to fetch");
+    return await res.json();
+}
+
+export async function fetchDatabase() {
+    const res = await fetch("http://127.0.0.1:8001/api/v1/local/database");
+    if (!res.ok) throw new Error("Failed to fetch");
+    return await res.json();
+}
+
+export async function fetchTask() {
+    const res = await fetch("http://127.0.0.1:8001/api/v1/local/tasl");
+    if (!res.ok) throw new Error("Failed to fetch");
+    return await res.json();
+}
